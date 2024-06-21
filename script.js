92716c419119f745967a04183033e2231972217c
Tristen Roupp <tristenr@usf.edu>
2024-06-21 17:29:44 -0400

    Changed the color back to original

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//U61753810
 const data = [100, 420, 230, 850, 560, 925];
 
 const margin = { top: 20, right: 30, bottom: 30, left: 40 };
@@ -11,35 +12,33 @@ const svg = d3.select("#chart")
   .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
- const x = d3.scaleLinear()
+const x = d3.scaleLinear()
     .domain([0, d3.max(data)])
     .range([50, width]);
 
- const bar = svg.selectAll(".bar")
+const bar = svg.selectAll(".bar")
     .data(data)
   .enter().append("g")
     .attr("class", "bar")
     .attr("transform", (d, i) => `translate(0,${i * (barHeight + 1)})`);
 
- bar.append("rect")
+bar.append("rect")
     .attr("width", 0) // Start with width 0 for transition
     .attr("height", barHeight)
     .transition()
     .duration(1000)
     .attr("width", d => x(d));
 
- bar.append("text")
+bar.append("text")
     .attr("x", d => x(d) - 3)
     .attr("y", barHeight / 2)
     .attr("dy", ".35em")
     .text(d => d);
 
-    bar.on("mouseover", function(event, d) {
-        console.log("Mouseover event triggered");
-        d3.select(this).select("rect").attr("fill", "purple");
-    })
-    .on("mouseout", function(event, d) {
-        console.log("Mouseout event triggered");
-        d3.select(this).select("rect").attr("fill", "thistle");
-    });
-    
+bar.on("mouseover", function(event, d) {
+    d3.select(this).select("rect").attr("fill", "orange");
+})
+.on("mouseout", function(event, d) {
+    d3.select(this).select("rect").attr("fill", "steelblue");
+});
+
