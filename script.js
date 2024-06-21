436d67a629bb2234f15bfcb47c6e0cb91e0220ed
Tristen Roupp <tristenr@usf.edu>
2024-06-21 16:50:47 -0400

    Implement transitions and hover effects for bars

--- script.js
+++ script.js
@@ -14,13 +14,19 @@ const svg = d3.select("#chart")
     const x = d3.scaleLinear()
     .domain([0, d3.max(data)])
     .range([50, width]);
-    
+
     const bar = svg.selectAll("g")
     .data(data)
   .enter().append("g")
     .attr("transform", (d, i) => `translate(0,${i * (barHeight + 1)})`);
 
-bar.append("rect")
+ bar.append("rect")
     .attr("width", d => x(d))
     .attr("height", barHeight);
+
+    bar.append("text")
+    .attr("x", d => x(d) - 3)
+    .attr("y", barHeight / 2)
+    .attr("dy", ".35em")
+    .text(d => d);
 
