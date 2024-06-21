727a855ff853d4e1e31b1008e8e081c617d9fa85
Tristen Roupp <tristenr@usf.edu>
2024-06-21 16:48:09 -0400

    Initialize SVG container with specified dimensions

---
+++ script.js
@@ -0,0 +1,12 @@
+const data = [100, 420, 230, 850, 560, 925];
+
+const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+const width = 500 - margin.left - margin.right;
+const barHeight = 20;
+const height = (barHeight + 1) * data.length;
+
+const svg = d3.select("#chart")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
